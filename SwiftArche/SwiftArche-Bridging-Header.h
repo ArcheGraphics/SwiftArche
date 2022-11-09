c86fd187f89af95c7dd522194022bd423de0c953
yangfengzzz <yangfengzzz@hotmail.com>
2022-11-09 08:44:42 +0800

    Initial Commit

---
+++ SwiftArche/SwiftArche-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  SwiftArche-Bridging-Header.h
+//  SwiftArche
+//
+//  Created by yangfengzz on 2022/11/9.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
