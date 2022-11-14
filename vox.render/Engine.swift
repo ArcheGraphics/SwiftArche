f57b53090e871386386eba1b1f44b58be2fad7f8
yangfeng <yangfengzzz@hotmail.com>
2022-11-14 21:27:16 +0800

    add main loop

--- vox.render/Engine.swift
+++ vox.render/Engine.swift
@@ -15,8 +15,8 @@ public class Engine {
     var sceneManager: SceneManager!
     var canvas: Canvas
     var device: MTLDevice
-    var _macroCollection: ShaderMacroCollection = ShaderMacroCollection();
-
+    var _macroCollection: ShaderMacroCollection = ShaderMacroCollection()
+        
     init(canvas: Canvas) {
         self.canvas = canvas
         guard let device = MTLCreateSystemDefaultDevice() else {
@@ -24,5 +24,42 @@ public class Engine {
         }
         self.device = device
         sceneManager = SceneManager(engine: self)
+    }
+    
+    /// Update the engine loop manually. If you call engine.run(), you generally don't need to call this function.
+    func update(_ deltaTime: Float) {
+        let scene = sceneManager._activeScene
+        let componentsManager = _componentsManager
+        if (scene != nil) {
+            scene!._activeCameras.sort { camera1, camera2 in
+                camera1.priority > camera2.priority
+            }
+            
+            componentsManager.callScriptOnStart()
+            // physicsManager._initialized && physicsManager._update(deltaTime / 1000.0)
+            // inputManager._update()
+            componentsManager.callScriptOnUpdate(deltaTime)
+            // componentsManager.callAnimationUpdate(deltaTime)
+            componentsManager.callScriptOnLateUpdate(deltaTime)
+            _render(scene!, deltaTime)
+        }
+        componentsManager.handlingInvalidScripts()
+    }
+    
+    func _render(_ scene: Scene, _ deltaTime: Float) {
+        let cameras = scene._activeCameras
+        _componentsManager.callRendererOnUpdate(deltaTime)
+        
+        scene._updateShaderData()
+        
+        if (cameras.count > 0) {
+            for camera in cameras {
+                _componentsManager.callCameraOnBeginRender(camera)
+                camera.render()
+                _componentsManager.callCameraOnEndRender(camera)
+            }
+        } else {
+            logger.debug("NO active camera.")
+        }
     }
 }
