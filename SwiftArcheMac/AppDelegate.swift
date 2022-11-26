a5e09dfe07891f754b4f9c8f72cee4d52576f31c
yangfengzzz <yangfengzzz@hotmail.com>
2022-11-26 17:21:34 +0800

    add mac project

---
+++ SwiftArcheMac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SwiftArcheMac
+//
+//  Created by yangfengzz on 2022/11/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
