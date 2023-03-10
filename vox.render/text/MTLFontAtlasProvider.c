612a07967a82eea2d8fb12b56fda39326019f9c9
yangfengzzz <yangfengzzz@hotmail.com>
2023-03-10 17:59:18 +0800

    add ozz animation

--- vox.render/text/MTLFontAtlasProvider.c
+++ vox.render/text/MTLFontAtlasProvider.c
@@ -8,13 +8,15 @@
 #include <math.h>
 #include <stdbool.h>
 
-float* createSignedDistanceFieldForGrayscaleImage(const uint8_t *imageData, size_t width, size_t height) {
+float *createSignedDistanceFieldForGrayscaleImage(const uint8_t *imageData, size_t width, size_t height) {
     if (imageData == NULL || width == 0 || height == 0)
         return NULL;
 
-    typedef struct { unsigned short x, y; } intpoint_t;
+    typedef struct {
+        unsigned short x, y;
+    } intpoint_t;
 
-    float* distanceMap = malloc(width * height * sizeof(float)); // distance to nearest boundary point map
+    float *distanceMap = malloc(width * height * sizeof(float)); // distance to nearest boundary point map
     intpoint_t *boundaryPointMap = malloc(width * height * sizeof(intpoint_t)); // nearest boundary point map
 
     // Some helpers for manipulating the above arrays
@@ -27,54 +29,43 @@ float* createSignedDistanceFieldForGrayscaleImage(const uint8_t *imageData, size
     const float distDiag = sqrt(2);
 
     // Initialization phase: set all distances to "infinity"; zero out nearest boundary point map
-    for (long y = 0; y < height; ++y)
-    {
-        for (long x = 0; x < width; ++x)
-        {
+    for (long y = 0; y < height; ++y) {
+        for (long x = 0; x < width; ++x) {
             distance(x, y) = maxDist;
-            nearestpt(x, y) = (intpoint_t){ 0, 0 };
+            nearestpt(x, y) = (intpoint_t) {0, 0};
         }
     }
 
     // Immediate interior/exterior phase: mark all points along the boundary as such
-    for (long y = 1; y < height - 1; ++y)
-    {
-        for (long x = 1; x < width - 1; ++x)
-        {
+    for (long y = 1; y < height - 1; ++y) {
+        for (long x = 1; x < width - 1; ++x) {
             bool inside = image(x, y);
             if (image(x - 1, y) != inside ||
-                image(x + 1, y) != inside ||
-                image(x, y - 1) != inside ||
-                image(x, y + 1) != inside)
-            {
+                    image(x + 1, y) != inside ||
+                    image(x, y - 1) != inside ||
+                    image(x, y + 1) != inside) {
                 distance(x, y) = 0;
-                nearestpt(x, y) = (intpoint_t){ x, y };
+                nearestpt(x, y) = (intpoint_t) {x, y};
             }
         }
     }
 
     // Forward dead-reckoning pass
-    for (long y = 1; y < height - 2; ++y)
-    {
-        for (long x = 1; x < width - 2; ++x)
-        {
-            if (distanceMap[(y - 1) * width + (x - 1)] + distDiag < distance(x, y))
-            {
+    for (long y = 1; y < height - 2; ++y) {
+        for (long x = 1; x < width - 2; ++x) {
+            if (distanceMap[(y - 1) * width + (x - 1)] + distDiag < distance(x, y)) {
                 nearestpt(x, y) = nearestpt(x - 1, y - 1);
                 distance(x, y) = hypot(x - nearestpt(x, y).x, y - nearestpt(x, y).y);
             }
-            if (distance(x, y - 1) + distUnit < distance(x, y))
-            {
+            if (distance(x, y - 1) + distUnit < distance(x, y)) {
                 nearestpt(x, y) = nearestpt(x, y - 1);
                 distance(x, y) = hypot(x - nearestpt(x, y).x, y - nearestpt(x, y).y);
             }
-            if (distance(x + 1, y - 1) + distDiag < distance(x, y))
-            {
+            if (distance(x + 1, y - 1) + distDiag < distance(x, y)) {
                 nearestpt(x, y) = nearestpt(x + 1, y - 1);
                 distance(x, y) = hypot(x - nearestpt(x, y).x, y - nearestpt(x, y).y);
             }
-            if (distance(x - 1, y) + distUnit < distance(x, y))
-            {
+            if (distance(x - 1, y) + distUnit < distance(x, y)) {
                 nearestpt(x, y) = nearestpt(x - 1, y);
                 distance(x, y) = hypot(x - nearestpt(x, y).x, y - nearestpt(x, y).y);
             }
@@ -82,27 +73,21 @@ float* createSignedDistanceFieldForGrayscaleImage(const uint8_t *imageData, size
     }
 
     // Backward dead-reckoning pass
-    for (long y = height - 2; y >= 1; --y)
-    {
-        for (long x = width - 2; x >= 1; --x)
-        {
-            if (distance(x + 1, y) + distUnit < distance(x, y))
-            {
+    for (long y = height - 2; y >= 1; --y) {
+        for (long x = width - 2; x >= 1; --x) {
+            if (distance(x + 1, y) + distUnit < distance(x, y)) {
                 nearestpt(x, y) = nearestpt(x + 1, y);
                 distance(x, y) = hypot(x - nearestpt(x, y).x, y - nearestpt(x, y).y);
             }
-            if (distance(x - 1, y + 1) + distDiag < distance(x, y))
-            {
+            if (distance(x - 1, y + 1) + distDiag < distance(x, y)) {
                 nearestpt(x, y) = nearestpt(x - 1, y + 1);
                 distance(x, y) = hypot(x - nearestpt(x, y).x, y - nearestpt(x, y).y);
             }
-            if (distance(x, y + 1) + distUnit < distance(x, y))
-            {
+            if (distance(x, y + 1) + distUnit < distance(x, y)) {
                 nearestpt(x, y) = nearestpt(x, y + 1);
                 distance(x, y) = hypot(x - nearestpt(x, y).x, y - nearestpt(x, y).y);
             }
-            if (distance(x + 1, y + 1) + distDiag < distance(x, y))
-            {
+            if (distance(x + 1, y + 1) + distDiag < distance(x, y)) {
                 nearestpt(x, y) = nearestpt(x + 1, y + 1);
                 distance(x, y) = hypot(x - nearestpt(x, y).x, y - nearestpt(x, y).y);
             }
@@ -110,10 +95,8 @@ float* createSignedDistanceFieldForGrayscaleImage(const uint8_t *imageData, size
     }
 
     // Interior distance negation pass; distances outside the figure are considered negative
-    for (long y = 0; y < height; ++y)
-    {
-        for (long x = 0; x < width; ++x)
-        {
+    for (long y = 0; y < height; ++y) {
+        for (long x = 0; x < width; ++x) {
             if (!image(x, y))
                 distance(x, y) = -distance(x, y);
         }
