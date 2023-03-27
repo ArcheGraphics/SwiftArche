d58d6a371ccac666cff4d46a9a0a54836f274148
yangfengzzz <yangfengzzz@hotmail.com>
2023-03-27 21:11:39 +0800

    add more render tech

--- vox.render/experimental/render-tech/XLightCuller.swift
+++ vox.render/experimental/render-tech/XLightCuller.swift
@@ -31,6 +31,29 @@ struct LightCullResult {
 
 /// Encapsulates the state for culling lights.
 class XLightCuller {
+    // Device from initialization.
+    var _device: MTLDevice
+
+    // For Tiled Light Culling on TBDR Apple Silicon GPUs.
+    var _renderCullingPipelineState: MTLRenderPipelineState!
+    var _pipelineStateHierarchical: MTLRenderPipelineState!
+    var _pipelineStateClustering: MTLRenderPipelineState!
+
+    var _initTilePipelineState: MTLRenderPipelineState!
+    var _depthBoundsTilePipelineState: MTLRenderPipelineState!
+
+    // For compute based light culling on tradional GPUs.
+    var _computeCullingPipelineState: MTLComputePipelineState!
+
+    // Common culling kenrels used on both traditional and TBDR GPUs.
+    var _hierarchicalClusteredPipelineState: MTLComputePipelineState!
+    var _spotCoarseCullPipelineState: MTLComputePipelineState!
+    var _pointCoarseCullPipelineState: MTLComputePipelineState!
+
+    var _lightCullingTileSize: Int
+    var _lightClusteringTileSize: Int
+
+
     /// Initializes this culling object, allocating compute pipelines.
     init(with device: MTLDevice,
          library: MTLLibrary,
@@ -38,11 +61,75 @@ class XLightCuller {
          useLightCullingTileShaders: Bool,
          lightCullingTileSize: Int,
          lightClusteringTileSize: Int) {
+        _device = device
+
+        _lightCullingTileSize = lightCullingTileSize
+        _lightClusteringTileSize = lightClusteringTileSize
+        rebuildPipelines(with: library, useRasterizationRate: useRasterizationRate, useLightCullingTileShaders: useLightCullingTileShaders)
     }
 
     func rebuildPipelines(with library: MTLLibrary,
                           useRasterizationRate: Bool,
                           useLightCullingTileShaders: Bool) {
+        var useRasterizationRate = useRasterizationRate
+        let fc = MTLFunctionConstantValues()
+        fc.setConstantValue(&useRasterizationRate, type: .bool, index: Int(XFunctionConstIndexRasterizationRate.rawValue))
+        fc.setConstantValue(&_lightCullingTileSize, type: .uint, index: Int(XFunctionConstIndexLightCullingTileSize.rawValue))
+        fc.setConstantValue(&_lightClusteringTileSize, type: .uint, index: Int(XFunctionConstIndexLightClusteringTileSize.rawValue))
+
+        if (useLightCullingTileShaders) {
+            let tilePipelineStateDescriptor = MTLTileRenderPipelineDescriptor()
+            let tilefc = fc.copy() as! MTLFunctionConstantValues
+
+            tilePipelineStateDescriptor.colorAttachments[0].pixelFormat = .r32Float
+            var tileSize = simd_uint2(UInt32(TILE_SHADER_WIDTH), UInt32(TILE_SHADER_HEIGHT))
+            var depthBoundsDispatchSize = simd_uint2(UInt32(TILE_DEPTH_BOUNDS_DISPATCH_SIZE), UInt32(TILE_DEPTH_BOUNDS_DISPATCH_SIZE))
+
+            tilefc.setConstantValue(&tileSize, type: .uint2, index: Int(XFunctionConstIndexTileSize.rawValue))
+            tilefc.setConstantValue(&depthBoundsDispatchSize, type: .uint2, index: Int(XFunctionConstIndexDispatchSize.rawValue))
+
+            tilePipelineStateDescriptor.tileFunction = library.makeFunction(name: "tileInit")!
+            tilePipelineStateDescriptor.label = "tileInit"
+
+            _initTilePipelineState = try? _device.makeRenderPipelineState(tileDescriptor: tilePipelineStateDescriptor,
+                    options: MTLPipelineOption(), reflection: nil)
+
+            tilePipelineStateDescriptor.tileFunction = try! library.makeFunction(name: "tileDepthBounds", constantValues: tilefc)
+            tilePipelineStateDescriptor.label = "tileDepthBounds"
+            _depthBoundsTilePipelineState = try? _device.makeRenderPipelineState(tileDescriptor: tilePipelineStateDescriptor,
+                    options: MTLPipelineOption(), reflection: nil)
+
+            tilePipelineStateDescriptor.tileFunction = try! library.makeFunction(name: "tileLightCulling", constantValues: tilefc)
+            tilePipelineStateDescriptor.label = "tileLightCulling"
+
+            _renderCullingPipelineState = try? _device.makeRenderPipelineState(tileDescriptor: tilePipelineStateDescriptor,
+                    options: MTLPipelineOption(), reflection: nil)
+
+            tilePipelineStateDescriptor.tileFunction = try! library.makeFunction(name: "tileLightCullingHierarchical", constantValues: tilefc)
+            tilePipelineStateDescriptor.label = "tileLightCullingHierarchical"
+            _pipelineStateHierarchical = try? _device.makeRenderPipelineState(tileDescriptor: tilePipelineStateDescriptor,
+                    options: MTLPipelineOption(), reflection: nil)
+            tilePipelineStateDescriptor.tileFunction = try! library.makeFunction(name: "tileLightClustering", constantValues: tilefc)
+            tilePipelineStateDescriptor.label = "tileLightClustering"
+            _pipelineStateClustering = try? _device.makeRenderPipelineState(tileDescriptor: tilePipelineStateDescriptor,
+                    options: MTLPipelineOption(), reflection: nil)
+        } else {
+            _computeCullingPipelineState = newComputePipelineState(library: library,
+                    functionName: "traditionalLightCulling",
+                    label: "LightCulling", functionConstants: fc)
+        }
+        _spotCoarseCullPipelineState = newComputePipelineState(library: library,
+                functionName: "kernelSpotLightCoarseCulling",
+                label: "SpotLightCulling",
+                functionConstants: fc)
+        _pointCoarseCullPipelineState = newComputePipelineState(library: library,
+                functionName: "kernelPointLightCoarseCulling",
+                label: "PointLightCulling",
+                functionConstants: fc)
+        _hierarchicalClusteredPipelineState = newComputePipelineState(library: library,
+                functionName: "traditionalLightClustering",
+                label: "LightClustring",
+                functionConstants: fc)
     }
 
     /// Initializes a LightCullResult object with buffers based on the view size and light counts.
